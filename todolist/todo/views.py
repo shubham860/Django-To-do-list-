a83fee61baf334f0f30a81d7838b2e691b10dc61
shubham860 <shubham.chauhan860@gmail.com>
2019-07-02 02:09:16 +0530

    update-11

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -7,11 +7,13 @@ from django.contrib import messages
 def homepage(request):
     if request.method == "POST":
         form = ListForm(request.POST)
+
         if form.is_valid():
             form.save()
             all_items = List.objects.all()
             messages.success(request,f"Item has been added to list!")
-            return render(request,'todo/home.html',{'all_items': all_items})
+            return render(request,'todo/home.html',{'all_items':all_items})
+
     else:
         all_items = List.objects.all()
-        return render(request,'todo/home.html',{'all_items': all_items})
+        return render(request,'todo/home.html',{'all_items':all_items})
